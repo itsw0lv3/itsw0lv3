98f57d4dcb0388c243e1c0f257068fdadb2ccfbd
wolve <65454356+itswolv3@users.noreply.github.com>
2021-07-04 18:08:41 +0200

    Add files via upload

---
+++ caesar_cipher/main.py
@@ -0,0 +1,35 @@
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    else:
+        end_text += char
+
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+print(art.logo)
+
+cont = True
+while cont:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    confirm = input("Would you like to go again? y/n?\n")
+    if confirm == "n":
+        cont = False
+        print("Goodbye.")
+
+shift =  shift % 26
